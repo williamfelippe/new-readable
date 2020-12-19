f266900ffa774d392e7d0cbfb5aa0cb971c9cdde
William Felippe <wfelippesilva@gmail.com>
2020-12-19 17:41:04 -0300

    Fixing thunks names

--- src/modules/post/slice/thunks.ts
+++ src/modules/post/slice/thunks.ts
@@ -4,25 +4,25 @@ import Vote from 'modules/vote/types/vote'
 import PostService from 'modules/post/services'
 
 export const fetchPosts = createAsyncThunk(
-  'category/fetchPosts', async () => {
+  'post/fetchPosts', async () => {
     return await PostService.getPosts()
   }
 )
 
 export const fetchByPostById = createAsyncThunk(
-  'category/fetchByPostById', async (postId: string) => {
+  'post/fetchByPostById', async (postId: string) => {
     return await PostService.getPost(postId)
   }
 )
 
 export const deletePost = createAsyncThunk(
-  'category/deletePost', async (postId: string) => {
+  'post/deletePost', async (postId: string) => {
     return await PostService.deletePost(postId)
   }
 )
 
 export const votePost = createAsyncThunk(
-  'category/votePost', async ({ postId, vote }: { postId: string, vote: Vote }) => {
+  'post/votePost', async ({ postId, vote }: { postId: string, vote: Vote }) => {
     return await PostService.votePost(postId, vote)
   }
 )
